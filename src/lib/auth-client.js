51696b0f96f82f6628ba7e617e923bb6721564f4
musfik070-gif <musfik070@gmail.com>
2026-06-29 05:27:40 +0600

    "update production api url"

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -4,6 +4,7 @@ const serverURL =
   process.env.NEXT_PUBLIC_API_URL ||
   process.env.NEXT_PUBLIC_SERVER_URL ||
   "http://localhost:5001";
+  
 
 export const authClient = createAuthClient({
   baseURL: serverURL.endsWith("/api/auth")
