0629ba463ce41894f1a383011bee5aa7375eb404
musfik070-gif <musfik070@gmail.com>
2026-06-29 07:57:25 +0600

    again the deployment google log in test

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -2,7 +2,7 @@ import { createAuthClient } from "better-auth/react";
 import { SERVER_URL } from "./api";
 
 export const authClient = createAuthClient({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  baseURL: `${process.env.NEXT_PUBLIC_API_URL}/api/auth`,
   fetchOptions: {
     credentials: "include"
   }
